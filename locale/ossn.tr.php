e60dd1f67ecbf3432b30817d05b11a6e44fad5ea
necocan <necocan_21@hotmail.com>
2020-06-29 23:34:00 +0300

    Create ossn.tr.php
    yeni dil eklendi

---
+++ locale/ossn.tr.php
@@ -0,0 +1,22 @@
+
+<?php
+/**
+ * Open Source Social Network
+ *
+ * @packageOpen Source Social Network
+ * @author    Open Social Website Core Team <info@informatikon.com>
+ * @copyright 2014 iNFORMATIKON TECHNOLOGIES
+ * @license   General Public Licence http://www.opensource-socialnetwork.org/licence
+ * @link      http://www.opensource-socialnetwork.org/licence
+ */
+$en = array(
+	'smtp' => 'SMTP',
+	'smtp:settings' => 'SMTP Ayarları',
+	'smtp:connection:connected' => 'SMTP Bağlantısı başarıyla kuruldu',
+	'smtp:connectio:failed' => 'SMTP Bağlantısı Başarısız Oldu',
+	'smtp:host' => "Mail Server (mail.yourdomain.com)",
+	'smtp:port' => 'Port',
+	'smtp:username' => 'Kullanıcı adı (name@yourdomain.com)',
+	'smtp:password' => 'Şifre',
+);
+ossn_register_languages('en', $en); 
